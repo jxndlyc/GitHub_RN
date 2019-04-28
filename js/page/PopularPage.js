f7fa05e00cdd76bb64f5f070b7063763b376e31f
liuyuchuan <liuyuchuan@dding.cn>
2019-04-28 21:48:44 +0800

    feat(all):top tab navigation

--- js/page/PopularPage.js
+++ js/page/PopularPage.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
 import {
     createMaterialTopTabNavigator,
     createAppContainer,
@@ -15,47 +15,83 @@ import {
 import NavigationUtil from "../navigator/NavigationUtil";
 import DetailPage from "./DetailPage";
 
-type Props = {};
+
 export default class PopularPage extends Component<Props> {
 
     constructor(props) {
         super(props)
-        this.tabNames = ['Java', 'Android', 'IOS', 'React', 'React Native', 'PHP'];
+        this.tabNames = ["Java", "Android", "IOS", "React", "React Native", "PHP"];
     }
 
-    _genTabs(){
-        const tabs = {};
-        this.tabNames.forEach((item, index) => {
-            tabs['tab${index}'] = {
-                screen:PopularTab,
-                navigationOptions:{
-                    title:item,
+    _genTabs() {
+        /*const tabs = {
+            Java:{
+                screen: props => <PopularTab {...props} tabLabel='Java'/>,
+                navigationOptions: {
+                    title: 'Java',
                 }
-            }
+            },Android:{
+                screen: props => <PopularTab {...props} tabLabel='Android'/>,
+                navigationOptions: {
+                    title: 'Android',
+                }
+            },IOS:{
+                screen: props => <PopularTab {...props} tabLabel='IOS'/>,
+                navigationOptions: {
+                    title: 'IOS',
+                }
+            },React:{
+                screen: props => <PopularTab {...props} tabLabel='React'/>,
+                navigationOptions: {
+                    title: 'React',
+                }
+            },React_Native:{
+                screen: props => <PopularTab {...props} tabLabel='React Native'/>,
+                navigationOptions: {
+                    title: 'React Native',
+                }
+            },PHP:{
+                screen: props => <PopularTab {...props} tabLabel='PHP'/>,
+                navigationOptions: {
+                    title: 'PHP',
+                }
+            },
+        };*/
+        const tabs = {};
+
+        this.tabNames.forEach((item, index) => {
+            console.log("index:" + index + ", item:" + item);
+            const  tab = "tab" + index;
+            tabs[`tab${index}`] = {
+                screen: props => <PopularTab {...props} tabLabel={item}/>,
+                navigationOptions: {
+                    title: item,
+                }
+            };
         });
+
         return tabs;
     }
 
     render() {
-        const TabNavigator = createAppContainer(createMaterialTopTabNavigator({
-                PopularTab1: {
-                    screen: PopularTab,
-                    navigationOptions: {
-                        title: "Tab1",
-                        tabBarLabel: "Tab1",
-                    }
-                },
-                PopularTab2: {
-                    screen: PopularTab,
-                    navigationOptions: {
-                        title: "Tab2",
-                        tabBarLabel: "Tab2",
-                    }
+        const TabNavigator = createAppContainer(createMaterialTopTabNavigator(
+            this._genTabs(), {
+                tabBarOptions: {
+                    tabStyle: styles.tabStyle,
+                    upperCaseLabel: false, //标签是否大写，默认为true
+                    scrollEnabled: true,//是否支持选项卡滑动，默认为false
+                    style: {
+                        backgroundColor: '#678'//tabBar颜色
+                    },
+                    indicatorStyle: styles.indicatorStyle,
+                    labelStyle: styles.labelStyle,
                 }
-            })
+            }
+            )
         );
 
-        return <View style={{flex: 1, marginTop: 0}}>
+        return <View style={{flex: 1, marginTop: 20}}>
+            <StatusBar backgroundColor={'#678'} translucent={true} animated={true} barStyle={'dark-content'}/>
             <TabNavigator/>
         </View>
 
@@ -88,6 +124,17 @@ const styles = StyleSheet.create({
     },
     welcome: {
         fontSize: 20,
+        textAlign: 'center',
+    },
+    tabStyle: {
+        minWidth: 30,
+    },
+    indicatorStyle: {
+        height: 2,
+        backgroundColor: 'white',
+    },
+    labelStyle: {
+        fontSize: 13,
         textAlign: 'center',
     },
 
